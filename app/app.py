2ecb5e49810c66d26bf530c9b8285f495f45c9c2
ulricht01 <99655536+ulricht01@users.noreply.github.com>
2025-06-15 23:07:25 +0200

    0.1.3
    Přidán jednoduchý "dashboard", který zatím zobrazuje pouze počty zpracovaných souborů.

--- app/app.py
+++ app/app.py
@@ -10,10 +10,32 @@ from dicttoxml import dicttoxml
 from xml.dom.minidom import parseString
 import xml.etree.ElementTree as ET
 import io
+import json
 
 from plugins.base_extractor import BaseExtractor
 
 available_extractors = {}
+dashboard_data = {
+        "none": 0        
+    }
+
+def prepare_dashboard_dict():
+    for i in available_extractors:
+        dashboard_data[i] = 0
+
+def report_dashboard(dashboard_data, output_path):
+    template_path = os.path.join(os.path.dirname(__file__), "..", "dashboard", "dashboard.html")
+    with open(template_path, "r", encoding="utf-8") as f:
+        template = f.read()
+        
+    html = template.replace("{{BARCHARTDATA}}", json.dumps(dashboard_data, ensure_ascii=False))
+
+    
+    output_file = os.path.join(output_path, "metadata_report.html")
+    with open(output_file, "w", encoding="utf-8") as f:
+        f.write(html)
+        
+    print(f"Dashboard vygenerován: {output_file}")
 
 def load_plugins():
     for loader, module_name, is_pkg in pkgutil.iter_modules(plugins.__path__):
@@ -26,6 +48,7 @@ def load_plugins():
                     if ext not in available_extractors:
                         available_extractors[ext] = instance
                         print(f"  Plugin '{attr.__name__}' zaregistrován pro příponu '{ext}'.")
+    prepare_dashboard_dict()
                         
 
 def extract_metadata(file_path):
@@ -33,10 +56,11 @@ def extract_metadata(file_path):
     
     if ext not in available_extractors:
         print(f"Žádný extraktor pro příponu: {ext}")
-    
+        dashboard_data["none"] +=1
     try:
         extractor = available_extractors[ext]
         metadata = extractor.extract_meta(file_path)
+        dashboard_data[ext] +=1
         return metadata
     except Exception as e:
         return {}
@@ -118,6 +142,7 @@ if __name__ == "__main__":
         print(f"{file}:")
         convert_to_xml(file, metadata, "exports")
         join_xml_files("exports", "G:/Programování/sipky/metadata-extractor/nested_exports/all.xml")
+        report_dashboard(dashboard_data, "G:/Programování/sipky/metadata-extractor/exports/reports")
 
 
     
